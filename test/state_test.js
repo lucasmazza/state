0165dcdb590c8d869c96c73c12091093a45855ec
Lucas Mazza <lucastmazza@gmail.com>
2014-06-24 23:20:21 -0300

    Use the canonical API instead of the Element extensions

--- test/state_test.js
+++ test/state_test.js
@@ -1,39 +1,39 @@
-/*global QUnit:true*/
+/*global QUnit:true,State:true*/
 var setup = {
   setup: function() {
     this.element = document.createElement('div');
   }
 };
 
-QUnit.module('Element#setState', setup);
+QUnit.module('State#set', setup);
 
 QUnit.test('it sets the state of an element through a class', function(assert) {
-  this.element.setState('loading');
+  State.set(this.element, 'loading');
 
   assert.haveClass(this.element, 'state-loading');
 });
 
 QUnit.test('it does not mantain multiple state classes at the same time', function(assert) {
-  this.element.setState('loading');
-  this.element.setState('done');
+  State.set(this.element, 'loading');
+  State.set(this.element, 'done');
 
   assert.haveClass(this.element, 'state-done');
   assert.notHaveClass(this.element, 'state-loading');
 });
 
-QUnit.module('Element#getState', setup);
+QUnit.module('State#get', setup);
 
 QUnit.test('gets the state of an element', function(assert) {
-  this.element.setState('loading');
-  assert.equal(this.element.getState(), 'loading');
+  State.set(this.element, 'loading');
+  assert.equal(State.get(this.element), 'loading');
 });
 
-QUnit.module('Element#clearState', setup);
+QUnit.module('State#clear', setup);
 
 QUnit.test('it removes the class when clearing the state of the element', function(assert) {
-  this.element.setState('loading');
+  State.set(this.element, 'loading');
   assert.haveClass(this.element, 'state-loading');
 
-  this.element.clearState();
+  State.clear(this.element);
   assert.notHaveClass(this.element, 'state-loading');
 });
